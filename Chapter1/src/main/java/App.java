bc07786cc34750c2af966016064e11669bc199d6
Halil Zeybek <halilzeybek@windowslive.com>
2023-10-10 22:36:00 +0200

    isUnique problem

--- Chapter1/src/main/java/App.java
+++ Chapter1/src/main/java/App.java
@@ -1,6 +1,28 @@
+import java.util.HashMap;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Chapter1 solutions");
+        //To check if the given string has all unique characters
+        String input = "Abcdef";
+        System.out.println(isUniqueChars(input));
+
+    }
+
+
+    static boolean isUniqueChars(String input){
+        boolean[] check = new boolean[128];
+
+        for (int i=0; i<input.length();i++){
+            int ascii_char = input.charAt(i);
+            if (check[ascii_char]){
+                return false;
+            }
+            check[ascii_char]=true;
+        }
+
+
+        return true;
+
     }
 }
